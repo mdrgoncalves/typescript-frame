52acb8bc438b17b6223881591315d6c44525e19f
mdrgoncalves <mdr.rgon@gmail.com>
2022-10-18 13:12:24 -0300

    Conclusão do Módulo I

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,17 @@
 export class Negociacao {
-    private _data;
-    private _quantidade;
-    private _valor;
 
-    constructor(data, quantidade, valor) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+    constructor(
+        private _data: Date, 
+        public readonly quantidade: number, 
+        public readonly valor: number
+    ) {}
+
+    get data(): Date {
+        const data = new Date(this._data.getTime());
+        return data;
     }
 
-    get data() {
-        return this._data;
-    }
-
-    get quantidade() {
-        return this._quantidade;
-    }
-
-    get valor() {
-        return this._valor;
-    }
-
-    get volume() {
-        return this._quantidade * this._valor;
+    get volume(): number {
+        return this.quantidade * this.valor;
     }
 }
