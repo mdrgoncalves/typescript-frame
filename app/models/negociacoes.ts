5cdaa330b255e6194562491fafc36e078df486ed
mdrgoncalves <mdr.rgon@gmail.com>
2022-10-19 13:59:55 -0300

    Topico 3 do Modulo 2

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -3,7 +3,7 @@ import { Negociacao } from './negociacao.js';
 export class Negociacoes {
     private negociacoes: Negociacao[] = [];
 
-    adiciona(negociacao: Negociacao): void {
+    adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
 
